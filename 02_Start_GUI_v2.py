6c8128496a3a1e5d780d2137f56ac7738565e7f0
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-03-18 12:19:47 +1300

    Start GUI Iteration 2 not working at all

--- 02_Start_GUI_v2.py
+++ 02_Start_GUI_v2.py
@@ -35,22 +35,22 @@ class Start:
         # Buttons go here...
         button_font = "Arial 12 bold"
         # Orange low stakes button...
-        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)",
+        self.low_stakes_button = Button(self.stakes_frame, text="Low ($5)",
                                        command=lambda: self.to_game(1),
                                        font=button_font, bg="#FF9933")
-        self.lowstakes_button.grid(row=0, column=0, pady=10)
+        self.low_stakes_button.grid(row=0, column=0, pady=10)
 
         # Yellow medium stakes button...
-        self.medstakes_button = Button(self.stakes_frame, text="Medium ($10)",
+        self.medium_stakes_button = Button(self.stakes_frame, text="Medium ($10)",
                                        command=lambda: self.to_game(2),
                                        font=button_font, bg="#FFFF33")
-        self.medstakes_button.grid(row=0, column=1, padx=5, pady=10)
+        self.medium_stakes_button.grid(row=0, column=1, padx=5, pady=10)
 
         # Green high stakes button...
-        self.highstakes_button = Button(self.stakes_frame, text="High ($15)",
+        self.high_stakes_button = Button(self.stakes_frame, text="High ($15)",
                                        command=lambda: self.to_game(3),
                                        font=button_font, bg="#99FF33")
-        self.highstakes_button.grid(row=0, column=2, pady=10)
+        self.high_stakes_button.grid(row=0, column=2, pady=10)
 
         # Help Button
         self.help_button = Button(self.start_frame, text="How to Play",
@@ -59,12 +59,56 @@ class Start:
 
     def to_game(self, stakes):
         starting_balance = self.start_amount_entry.get()
-        Game(self, stakes, starting_balance)
+
+        # Set error background colours and assume that there are no
+        # errors at the start...
+        error_back = "#ffafaf"
+        has_errors = "no"
+
+        # change background to white (for testing purposes)...
+        self.start_amount_entry.config(bg="white")
+        self.amount_error_label.config(text="")
+
+        try:
+            starting_balance = int(starting_balance)
+
+            if starting_balance < 5:
+                has_errors = "yes"
+                error_feedback = "Sorry the least you " \
+                                 "can play with is $5 "
+
+            elif starting_balance > 50:
+                has_errors = "yes"
+                error_feedback = "Too high! The most you can risk in this " \
+                                 "game is $50"
+            elif starting_balance < 10 and (stakes == 2 or stakes == 3):
+                has_errors = "yes"
+                error_feedback = "Sorry you can only afford to " \
+                                 "play a low stakes game."
+            elif starting_balance < 15 and stakes == 3:
+                has_errors = "yes"
+                error_feedback = "Sorry, you can only afford to " \
+                                 "play a low or medium stakes game."
+        except ValueError:
+            has_errors = "yes"
+            error_feedback = "Please enter a dollar amount (no text / decimals)"
+
+        if has_errors == "yes":
+            self.start_amount_entry.config(bg=error_back)
+            self.amount_error_label.config(text=error_feedback)
+
+        else:
+            Game(self, stakes, starting_balance)
+
+            # hide start up window
+            # root.withdraw()
+
 
 class Game:
     def __int__(self, partner, stakes, starting_balance):
         print(stakes)
         print(starting_balance)
+
 
 if __name__ == '__main__':
     class Game:
