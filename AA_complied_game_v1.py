2bbf0799b9fa3f75a4f509e320f59f7461651f34
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-03-23 14:21:12 +1300

    Play GUI video done

--- AA_complied_game_v1.py
+++ AA_complied_game_v1.py
@@ -151,18 +151,53 @@ class Start:
         root.withdraw()
 
 
+
 class Game:
-    def __int__(self, partner, stakes, starting_balance):
+    def __init__(self, partner, stakes, starting_balance):
         print(stakes)
         print(starting_balance)
 
+        # initialise variables
+        self.balance = IntVar()
 
-if __name__ == '__main__':
-    class Game:
-        def __init__(self, partner, stakes, starting_balance):
-            print(stakes)
-            print(starting_balance)
+        # Set starting balance to amount entered by user of game
+        self.balance.set(starting_balance)
 
+        # GUI Setup
+        self.game_box = Toplevel()
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # Heading row (row 0)
+        self.heading_label = Label(self.game_frame, text="Heading",
+                                   font="Arial 24 bold", padx=10,
+                                   pady=10)
+        self.heading_label.grid(row=0)
+
+        # Balance Label
+        self.balance_frame = Frame(self.game_frame)
+        self.balance_frame.grid(row=1)
+
+        self.balance_label = Label(self.game_frame, text="Balance...")
+        self.balance_label.grid(row=2)
+
+        self.play_button = Button(self.game_frame, text="Gain",
+                                  padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=3)
+
+    def reveal_boxes(self):
+        # retrieval the balance from the initial function..
+        current_balance = self.balance.get()
+
+        # Adjust the blanace (subtract game cost and add pay out)
+        # For testing purposes, just add 2
+        current_balance += 2
+
+        # Set balance to adjusted balance
+        self.balance.set(current_balance)
+
+        # Edit label so user can see their balance
+        self.balance_label.configure(text="Balance: {}".format(current_balance))
 
 # main routine
 if __name__ == "__main__":
