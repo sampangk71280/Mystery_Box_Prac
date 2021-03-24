ca4b99ff482428a19a6926c573ddc9b64d3ef811
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-03-25 12:06:27 +1300

    Add files via upload

--- 05_Game_Playable_v2.py
+++ 05_Game_Playable_v2.py
@@ -11,7 +11,8 @@ class Start:
         self.start_frame.grid()
 
 
-        self.push_me_button = Button(text="Push Me", command=self.to_game)
+        self.push_me_button = Button(self.start_frame, text="Push Me",
+                                     command=self.to_game)
         self.push_me_button.grid(row=0, pady=10)
 
     def to_game(self):
@@ -23,8 +24,7 @@ class Start:
         Game(self, stakes, starting_balance)
 
         # hide start up window
-        root.withdraw()
-
+        self.start_frame.destroy()
 
 
 class Game:
@@ -43,6 +43,10 @@ class Game:
 
         # GUI Setup
         self.game_box = Toplevel()
+
+        # If users press cross at top, game quits
+        self.game_box.protocol('WM_DELETE_WINDOW', self.to_quit)
+
         self.game_frame = Frame(self.game_box)
         self.game_frame.grid()
 
@@ -61,29 +65,35 @@ class Game:
         self.instructions_label.grid(row=1)
 
         # Boxes go here (row 2)
-        box_text = "Arial 16 bold"
-        box_back = "#b6ea96" # light green
-        box_width = 5
+
         self.box_frame = Frame(self.game_frame)
         self.box_frame.grid(row=2, pady=10)
 
+        photo = PhotoImage(file="question.gif")
 
-        self.prize1_label = Label(self.box_frame, text="?\n", font=box_text,
-                               bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize1_label = Label(self.box_frame, image=photo,
+                                  padx=10, pady=10)
+        self.prize1_label.photo= photo
         self.prize1_label.grid(row=0, column=0)
 
-        self.prize2_label = Label(self.box_frame, text="?\n", font=box_text,
-                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize2_label = Label(self.box_frame, image=photo,
+                                  padx=10, pady=10)
+        self.prize2_label.photo = photo
         self.prize2_label.grid(row=0, column=1, padx=10)
 
-        self.prize3_label = Label(self.box_frame, text="?\n", font=box_text,
-                                  bg=box_back, width=box_width, padx=10, pady=10)
+        self.prize3_label = Label(self.box_frame, image=photo,
+                                  padx=10, pady=10)
+        self.prize3_label.photo = photo
         self.prize3_label.grid(row=0, column=2)
 
         # play button goes here (row 3)
         self.play_button = Button(self.game_frame, text="Open Boxes",
                                   bg="#FFFF33", font="Arial 15 bold", width=20,
                                   padx=10, pady=10, command=self.reveal_boxes)
+
+        # bind button to <enter> (users can push enter to reveal the boxes)
+        self.play_button.focus()
+        self.play_button.bind('<Return>', lambda e: self.reveal_boxes())
         self.play_button.grid(row=3)
 
         # Balance Label (row 4)
@@ -104,6 +114,16 @@ class Game:
                                   bg="#808080", fg="white")
         self.help_button.grid(row=0, column=0, padx=2)
 
+        self.stats_button = Button(self.help_export_frame, text="Game Stats...",
+                                   font="Arial 15 bold", bg="#006633", fg="white")
+        self.stats_button.grid(row=0, column=1, padx=2)
+
+        # Quit Button
+        self.quit_button = Button(self.game_frame, text="Quit", fg="white",
+                                  bg="#660000", font="Arial 15 bold", width=20,
+                                  command=self.to_quit, padx=10, pady=10)
+        self.quit_button.grid(row=6, pady=10)
+
     def reveal_boxes(self):
         # retrieval the balance from the initial function..
         current_balance = self.balance.get()
@@ -116,27 +136,32 @@ class Game:
             prize_num = random.randint (1,100)
 
             if 0 < prize_num <= 5:
-                prize = "gold\n(${})".format(5 * stakes_multiplier)
+                prize = PhotoImage(file="gold.gif")
+                prize_list = "gold\n(${})".format(5 * stakes_multiplier)
                 round_winnings += 5 * stakes_multiplier
-                back_colour = "#CEA935" # Gold colours
             elif 5 < prize_num <= 25:
-                prize = "silver\n(${})".format(2 * stakes_multiplier)
+                prize = PhotoImage(file="silver.gif")
+                prize_list = "silver\n(${})".format(2 * stakes_multiplier)
                 round_winnings += 2 * stakes_multiplier
-                back_colour = "#B7B7B5" # Silver colour
             elif 25 < prize_num <= 65:
-                prize = "copper\n(${})".format(1 * stakes_multiplier)
+                prize = PhotoImage(file="copper.gif")
+                prize_list = "copper\n(${})".format(1 * stakes_multiplier)
                 round_winnings += 1 * stakes_multiplier
-                back_colour = "#BC7F61" # copper colour
             else:
-                prize = "lead\n($0)"
+                prize = PhotoImage(file="lead.gif")
+                prize_list = "lead\n($0)"
 
             prizes.append(prize)
-            backgrounds.append(back_colour)
+            stats_prizes.append(prize_list)
+
+        photo1 = prizes[0]
+        photo2 = prizes[1]
+        photo3 = prizes[2]
 
         # Display prizes...
-        self.prize1_label.config(text=prizes[0], bg)
-        self.prize2_label.config(text=prizes[1])
-        self.prize3_label.config(text=prizes[2])
+        self.prize1_label.config(image=photo1)
+        self.prize2_label.config()
+        self.prize3_label.config(text=prizes[2], bg=backgrounds[2])
 
         # deduct cost of game
         current_balance -= 5 * stakes_multiplier
@@ -153,6 +178,21 @@ class Game:
 
         # edit label so user can see their balance
         self.balance_label.config(text=balance_statement)
+
+        if current_balance < 5 * stakes_multiplier:
+            self.play_button.config(state=DISABLED)
+            self.game_box.focus()
+            self.play_button.config(text="Game Over")
+
+            balance_statement = "Current Balance: ${}\n" \
+                                "Your balance is too low. You can only quit " \
+                                "or view your stats. Sorry about that.".format(current_balance)
+            self.balance_label.config(fg="#660000", font="Arial 10 bold",
+                                      text=balance_statement)
+    def to_quit(self):
+        root.destroy()
+
+
 
 # main routine
 if __name__ == "__main__":
